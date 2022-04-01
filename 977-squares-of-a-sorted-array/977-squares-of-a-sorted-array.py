de158256b418372b621881721d018c38b3593886
Jason Zhao <94204700+jasonz23@users.noreply.github.com>
2022-04-01 14:14:30 -0700

    Time: 334 ms (42.99%), Space: 16.3 MB (53.09%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
@@ -4,5 +4,12 @@ class Solution:
         for n in nums:
             na.append(n**2)
             
+        # n = len(nums)
+        # for i in range(n-1):
+        #     for j in range(0,n- i - 1):
+        #         if na[j] > na[j + 1]:
+        #             na[j], na[j + 1] = na[j + 1], na[j]
+        # return na
         return sorted(na)
+                
         
