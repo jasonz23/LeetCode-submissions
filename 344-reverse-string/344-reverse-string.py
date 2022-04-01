e810ec19f6cab7154192eb47e85f59870e87683e
Jason Zhao <94204700+jasonz23@users.noreply.github.com>
2022-04-01 12:19:14 -0700

    Time: 308 ms (33.99%), Space: 18.5 MB (48.50%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -3,13 +3,13 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        s.reverse()
-        # l = len(s)
-        # print(a)
-        # for n in range(l):
-        #     del s[0]
-        #     #print(a[n])
-        #     #s.append(a[n])
+        for i in range (0,math.floor(len(s)/2)): #create a loop variable from 0 to mid ength of the loop
+            b= s[len(s)-1-i];  #asssign a temp the value of i th index from the end and swap th e ith, last ith values
+            s[len(s)-1-i] = s[i] 
+            s[i] = b
+
+            
+
             
 
 
